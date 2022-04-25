82bacaf379e0ac2c2fdbbea2de7dcc9e4b82ce19
yerkebulanauelbek <auelbekov2002@gmail.com>
2022-04-26 00:42:15 +0600

    1draft

--- TechnoGet/technoget_front/src/app/app.module.ts
+++ TechnoGet/technoget_front/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryProductListComponent } from './category-product-list/category-product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    FooterComponent,
+    CategoryListComponent,
+    CategoryProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
