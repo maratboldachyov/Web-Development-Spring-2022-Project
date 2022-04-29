4ce24b8cae1a63045f554230cae872c95cea6d65
maratboldachyov <74499901+maratboldachyov@users.noreply.github.com>
2022-04-30 03:06:34 +0600

    Project. Front. 4 Draft

--- TechnoGet/technoget_front/src/app/cart/cart.component.ts
+++ TechnoGet/technoget_front/src/app/cart/cart.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+import {PRODUCTS} from "../fake_db";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
 
-  constructor() { }
+  items = this.cartService.getItems();
+  products = PRODUCTS;
 
-  ngOnInit(): void {
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+  // onDelete(): void {
+  //   this.items = this.cartService.clearCart();
+  // }
+  clearCart() {
+    this.items = [];
+    return this.items;
   }
 
 }
+
